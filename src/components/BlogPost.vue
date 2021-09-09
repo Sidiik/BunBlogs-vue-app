22551c2c85bf25c7efe2223aed6deef76b7f3852
Sadiq O <sadiqomar890@gmail.com>
2021-09-09 23:10:42 +0300

    made changes

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <h2 v-if="post.welcomeScreen">
+      {{ post.title }}
+    </h2>
+    <p>{{ post.blog }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogPost",
+  props: ["post"],
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,24 @@
-<template> </template>
+<template>
+  <BlogPost :post="welcomeScreen" />
+</template>
 
 <script>
+import BlogPost from "../components/BlogPost.vue";
 export default {
   name: "Home",
+  components: {
+    BlogPost,
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Hey this is title screen",
+        welcomeScreen: true,
+        blog:
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,placeat. Ipsa corporis fuga quos quae dignissimos cumque. Vitae eni cupiditate excepturi sed ex facere voluptatibus eum cum ab.",
+      },
+    };
+  },
 };
 </script>
 
